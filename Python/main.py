0b22afc8b4f01f4dd810ab93a58523cb4fcb6bf9
jweken <jweken@kpnmail.nl>
2022-06-15 16:16:57 +0200

    moved more pythn files

--- Python/main.py
+++ Python/main.py
@@ -1,11 +1,10 @@
 from src.Compiler import CompileMe
-from syspaths import ShowSysPaths as path
+from src.syspaths import ShowSysPaths as path
 
 
 print()
 print("De paden van het systeem:")
 print()
-
 path()
 
 print()
